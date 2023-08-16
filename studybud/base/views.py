26003f8dc125d3ab71910666087ecefbe3c69856
benicastro <benedict.zcastro@gmail.com>
2023-08-16 23:17:26 +0800

    Finalized the mobile responsiveness of the website

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -69,7 +69,7 @@ def home(request):
         Q(topic__name__icontains=q) | Q(name__icontains=q) | Q(description__icontains=q)
     )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -201,5 +201,11 @@ def updateUser(request):
 
 
 def topicsPage(request):
-    topics = Topic.objects.filter()
+    q = request.GET.get("q") if request.GET.get("q") != None else ""
+    topics = Topic.objects.filter(name__icontains=q)
     return render(request, "base/topics.html", {"topics": topics})
+
+
+def activitiesPage(request):
+    room_messages = Message.objects.all()
+    return render(request, "base/activities.html", {"room_messages": room_messages})
